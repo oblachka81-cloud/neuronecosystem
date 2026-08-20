a19c6ec3b07a039a3ad662c13429bdd744cb23d7
oblachka81-cloud <oblachka81@gmail.com>
2026-08-21 06:37:04 +0800

    Change liveInterval from '60m' to '1m'

--- js/exchange.js
+++ js/exchange.js
@@ -552,7 +552,7 @@ function candlesSvg(candles) {
 
 let liveChartSymbol = 'BTC';
 let liveChartCategory = 'crypto';
-let liveInterval = '60m';
+let liveInterval = '1m';
 const liveChartCache = {};
 const LIVE_ASSETS = { crypto: ['TON', 'BTC', 'XAUt0'], xstocks: ['AAPLx', 'NVDAx', 'TSLAx', 'AMZNx', 'SPYx'] };
 const LIVE_INTERVALS = [['1m', '1m'], ['5m', '5m'], ['60m', '1H'], ['1d', '1D']];
