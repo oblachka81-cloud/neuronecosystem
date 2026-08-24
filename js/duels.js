81ff0b52e1b884fea938f27f145f7eb39f96937b
oblachka81-cloud <oblachka81@gmail.com>
2026-08-24 22:30:59 +0800

    Hide header and footer on duel join
    Hide header and footer elements when removing containers.

--- js/duels.js
+++ js/duels.js
@@ -344,6 +344,11 @@ function duelStartBattle(duelIdParam, duelDataParam) {
   
   const joinContainer = document.getElementById('duelJoinContainer');
   if (joinContainer) joinContainer.remove();
+
+  const header = document.querySelector('.header');
+  const footer = document.querySelector('footer');
+  if (header) header.style.display = 'none';
+  if (footer) footer.style.display = 'none';
   
   duelRenderBattleScreen();
 }
