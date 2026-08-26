ec2cc27adfba063d78b6dbc2c575c76048066312
oblachka81-cloud <oblachka81@gmail.com>
2026-08-26 19:10:24 +0800

    Update haptic.js

--- js/haptic.js
+++ js/haptic.js
@@ -105,7 +105,7 @@ document.addEventListener('click', function (e) {
   
   if (el && !el.disabled && !el.classList.contains('no-haptic')) {
     setTimeout(() => {
-      if (navigator.vibrate) navigator.vibrate(60);
+      if (navigator.vibrate) navigator.vibrate(110);
       if (window.Telegram?.WebApp?.HapticFeedback) window.Telegram.WebApp.HapticFeedback.impactOccurred('light');
     }, 50);
   }
