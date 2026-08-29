f1df5ba9566a6b3157fd65de9cbcade59d3aef6c
oblachka81-cloud <oblachka81@gmail.com>
2026-08-29 20:58:31 +0800

    Refactor sendJetton function and add export

--- src/services/ton.js
+++ src/services/ton.js
@@ -1,6 +1,7 @@
 const { TonClient, WalletContractV4, internal, Address } = require('@ton/ton');
 const { mnemonicToPrivateKey } = require('@ton/crypto');
 const { beginCell } = require('@ton/core');
+const OPERATIONAL_WALLET = 'UQBniD_M-MTeVqUbWshZrXdQcz0m8lPstG3mQg1AL5KKCGSv';
 
 async function getJettonWalletAddress(client, ownerAddress, jettonMasterAddress) {
   const jettonWalletCode = await client.runMethod(jettonMasterAddress, 'get_wallet_address', [
@@ -11,12 +12,15 @@ async function getJettonWalletAddress(client, ownerAddress, jettonMasterAddress)
 
 async function sendCogniqJetton(toAddress, amount, privateKeyHex) {
   const COGNIQ_JETTON_MASTER = 'EQDOjRZ5rbSnBBvhsv4g0JNN67p89617_2pNc_AO1dTEkaNg';
-  
+  return await sendJetton(COGNIQ_JETTON_MASTER, toAddress, amount, privateKeyHex);
+}
+
+async function sendJetton(jettonMaster, toAddress, amount, privateKeyHex) {
   const client = new TonClient({
     endpoint: 'https://toncenter.com/api/v2/jsonRPC',
     apiKey: process.env.TON_CENTER_API_KEY || ''
   });
-  
+
   let keyPair;
   if (privateKeyHex.includes(' ')) {
     keyPair = await mnemonicToPrivateKey(privateKeyHex.split(' '));
@@ -27,22 +31,19 @@ async function sendCogniqJetton(toAddress, amount, privateKeyHex) {
       secretKey: keyBuffer
     };
   }
-  
-  const OPERATIONAL_WALLET = 'UQBniD_M-MTeVqUbWshZrXdQcz0m8lPstG3mQg1AL5KKCGSv';
+
   const walletAddress = Address.parse(OPERATIONAL_WALLET);
-  
-  const jettonWalletAddress = await getJettonWalletAddress(client, walletAddress, Address.parse(COGNIQ_JETTON_MASTER));
-  
+  const jettonWalletAddress = await getJettonWalletAddress(client, walletAddress, Address.parse(jettonMaster));
   const toAddr = Address.parse(toAddress);
-  
+
   const wallet = WalletContractV4.create({
     address: walletAddress,
     workchain: 0,
     publicKey: keyPair.publicKey
   });
-  
+
   const contract = client.open(wallet);
-  
+
   const jettonTransferBody = beginCell()
     .storeUint(0xf8a7ea5, 32)
     .storeUint(0, 64)
@@ -53,9 +54,9 @@ async function sendCogniqJetton(toAddress, amount, privateKeyHex) {
     .storeCoins(100000000)
     .storeBit(0)
     .endCell();
-  
+
   const seqno = await contract.getSeqno();
-  
+
   await contract.sendTransfer({
     seqno,
     secretKey: keyPair.secretKey,
@@ -67,13 +68,11 @@ async function sendCogniqJetton(toAddress, amount, privateKeyHex) {
       })
     ]
   });
-  
+
   await new Promise(resolve => setTimeout(resolve, 5000));
-  
+
   const transactions = await contract.getTransactions(1);
-  const txHash = transactions[0].hash().toString('hex');
-  
-  return txHash;
+  return transactions[0].hash().toString('hex');
 }
 
-module.exports = { getJettonWalletAddress, sendCogniqJetton };
+module.exports = { getJettonWalletAddress, sendCogniqJetton, sendJetton };
