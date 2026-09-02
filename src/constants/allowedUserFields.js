5d4150815ffeeb3c7e947528b4c8581c282777e9
oblachka81-cloud <oblachka81@gmail.com>
2026-09-02 19:53:40 +0800

    Update allowedUserFields.js

--- src/constants/allowedUserFields.js
+++ src/constants/allowedUserFields.js
@@ -1,6 +1,7 @@
 const ALLOWED_USER_FIELDS = new Set([
   'username', 'first_name', 'balance', 'games_today', 'last_game_date',
   'last_super_game_date', 'super_games_total', 'super_game_pending',
+  'granted_super_games',
   'referrer_id', 'referred_count', 'current_game_index', 'current_game_score',
   'current_question_order', 'current_hints_used', 'current_is_super',
   'question_start_time', 'withdraw_tickets',
@@ -9,6 +10,8 @@ const ALLOWED_USER_FIELDS = new Set([
   'streak_eternal_weeks',
   'recent_questions',
   'games_played_total',
+  'balance_purchased',
+  'super_replay_used',
 ]);
 
 module.exports = { ALLOWED_USER_FIELDS };
