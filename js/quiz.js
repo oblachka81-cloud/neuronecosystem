17dee97aeb9f357e2360c58a03fbc908320f1210
oblachka81-cloud <oblachka81@gmail.com>
2026-09-06 19:13:49 +0800

    Update avatar image source in quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -632,7 +632,7 @@ function renderLeaderboard(data) {
             ${myPhotoUrl ? `
             <div class="my-card-avatar">
               <img src="${myPhotoUrl}">
-              <img src="leaderboard/avatar_ring_wood.webp">
+              <img src="${FRAME_IMG[me.avatarFrame] || 'leaderboard/avatar_ring_wood.webp'}">
             </div>
             ` : ''}
           </div>
